f5f15b2aa8b5b08383631c5624e226c00daef2b6
gdorian486 <gueraud8(>
2021-02-25 10:21:32 +0100

    ajout d'autobus

--- src/com/tec/Autobus.java
+++ src/com/tec/Autobus.java
@@ -1,35 +1,73 @@
 package com.tec;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 class Autobus {
+    private final ArrayList<PassagerStandard> passagerStandards;
+    private Jauge jaugeAssise;
+    private Jauge jaugeDebout;
+    private int arretCourant;
 
     // constructor
     public Autobus(int nbPlaceAssise, int nbPlaceDebout) {
+        int placeTotal = nbPlaceAssise+nbPlaceDebout;
+        this.passagerStandards = new ArrayList<PassagerStandard>(placeTotal);
+        this.jaugeAssise = new Jauge(nbPlaceAssise,0);
+        this.jaugeDebout = new Jauge(nbPlaceDebout,0);
+        this.arretCourant = 0;
     }
 
     //methods
     public boolean aPlaceAssise() {
-        return false;
+        return this.jaugeAssise.estVert();
     }
 
     public boolean aPlaceDebout() {
-        return false;
+        return this.jaugeDebout.estVert();
     }
 
     public void monteeDemanderAssis(PassagerStandard p) {
+        this.jaugeAssise.incrementer();
+        p.changerEnAssis();
+        this.passagerStandards.add(p);
     }
 
     public void monteeDemanderDebout(PassagerStandard p) {
+        this.jaugeDebout.incrementer();
+        p.changerEnDebout();
+        this.passagerStandards.add(p);
     }
 
     public void allerArretSuivant() {
+        this.arretCourant ++;
+        Iterator<PassagerStandard> it = this.passagerStandards.iterator();
+        while(it.hasNext()) {
+            it.next().nouvelArret(this,this.arretCourant);
+        }
     }
 
     public void arretDemanderAssis(PassagerStandard p) {
+        this.jaugeDebout.decrementer();
+        this.jaugeAssise.incrementer();
+        p.changerEnAssis();
     }
 
     public void arretDemanderDebout(PassagerStandard p) {
+        this.jaugeAssise.decrementer();
+        this.jaugeDebout.incrementer();
+        p.changerEnDebout();
     }
 
     public void arretDemanderSortie(PassagerStandard p) {
+        if (p.estAssis()){
+            this.jaugeAssise.decrementer();
+            this.passagerStandards.remove(p);
+            p.changerEnDehors();
+        }else if(p.estDebout()){
+            this.jaugeDebout.decrementer();
+            this.passagerStandards.remove(p);
+            p.changerEnDehors();
+        }
     }
 }
--- src/com/tec/Main.java
+++ src/com/tec/Main.java
@@ -3,5 +3,8 @@ package com.tec;
 public class Main {
 
     public static void main(String[] args) {
+        Autobus auto1 = new Autobus(10,15);
+        Autobus auto2 = new Autobus(5,5);
+        auto1.allerArretSuivant();
     }
 }
