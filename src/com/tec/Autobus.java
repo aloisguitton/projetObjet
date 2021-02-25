bbf19187c1ea2720dedd75d02bea222b228c7e04
aloisguitton <aloisguitton@orange.fr>
2021-02-25 08:48:48 +0100

    Commit all files

---
+++ src/com/tec/Autobus.java
@@ -0,0 +1,35 @@
+package com.tec;
+
+class Autobus {
+
+    // constructor
+    public Autobus(int nbPlaceAssise, int nbPlaceDebout) {
+    }
+
+    //methods
+    public boolean aPlaceAssise() {
+        return false;
+    }
+
+    public boolean aPlaceDebout() {
+        return false;
+    }
+
+    public void monteeDemanderAssis(PassagerStandard p) {
+    }
+
+    public void monteeDemanderDebout(PassagerStandard p) {
+    }
+
+    public void allerArretSuivant() {
+    }
+
+    public void arretDemanderAssis(PassagerStandard p) {
+    }
+
+    public void arretDemanderDebout(PassagerStandard p) {
+    }
+
+    public void arretDemanderSortie(PassagerStandard p) {
+    }
+}
