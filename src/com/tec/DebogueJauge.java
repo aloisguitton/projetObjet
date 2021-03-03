93dc8d1bc25f18155c1883ddf1e490e104d3e15c
maxime <maxime.guillaume4@gmail.com>
2021-03-03 08:34:00 +0100

    debogueJauge

--- src/com/tec/DebogueJauge.java
+++ src/com/tec/DebogueJauge.java
@@ -3,12 +3,17 @@ package com.tec;
 public class DebogueJauge {
 
     public static void main(String args){
-        Jauge j1 = new Jauge(10,1);
+        Jauge j1 = new Jauge(5,0);
 
         System.out.println("estRouge : "+j1.estRouge());
         System.out.println("estVert : "+j1.estVert());
 
-
-
+        if(j1.estVert()){
+            j1.incrementer();
+        }
+        else if (j1.estRouge()){
+            j1.decrementer();
+        }
+        System.out.println(j1.toString());
     }
 }
