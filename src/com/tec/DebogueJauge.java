3f42eb9c9c13f10f734da7369d02dffd58858339
maxime <maxime.guillaume4@gmail.com>
2021-03-03 08:26:44 +0100

    debogueJauge created

---
+++ src/com/tec/DebogueJauge.java
@@ -0,0 +1,14 @@
+package com.tec;
+
+public class DebogueJauge {
+
+    public static void main(String args){
+        Jauge j1 = new Jauge(10,1);
+
+        System.out.println("estRouge : "+j1.estRouge());
+        System.out.println("estVert : "+j1.estVert());
+
+
+
+    }
+}
